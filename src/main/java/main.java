b9d9b0d2229c981592a1d1f7f7b638fb52dddbae
ahmed-fouad0440 <ahme0440@algonquinlive.com>
2022-11-29 21:39:38 -0500

    Initial

---
+++ src/main/java/main.java
@@ -0,0 +1,100 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fatou
+ */
+public class main {
+
+        public static void test_gitea(){
+            String prefix = "cmd /c ";
+            String gitStatus = "git status";
+            String gitInit = "git init";
+            String gotoDir = "cd C:\\new\\test1";
+            
+            
+            String pushFirstTime = "git add . && git commit -am \"Initial\" && git remote add origin git@github.com:Ahmed-Fouad0440/test.git\n" +
+                                   "git branch -M main\n"+
+                                   "git push -u origin main\n";
+        String command = prefix + gotoDir + " && " + pushFirstTime;
+ 
+try {
+    Process process = Runtime.getRuntime().exec(command);
+ 
+    BufferedWriter writer = new BufferedWriter(
+            new OutputStreamWriter(process.getOutputStream()));
+    writer.write("09-20-14");
+    writer.close();
+ 
+    BufferedReader reader = new BufferedReader(new InputStreamReader(
+            process.getInputStream()));
+    String line;
+    while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+    }
+    reader.close();
+} catch (IOException e) {
+    e.printStackTrace();
+}
+    }
+
+    
+    public static void main(String[] args) throws IOException {
+        test_gitea();
+    }
+        
+    public static void test1() throws IOException {        
+    
+        String command = "ping www.codejava.net";
+
+        Process process = Runtime.getRuntime().exec(command);
+       
+        
+    
+    BufferedReader reader = new BufferedReader(
+            new InputStreamReader(process.getInputStream()));
+    String line;
+    while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+    }
+ 
+    reader.close();
+ 
+    }
+    
+    
+    public static void test2(){
+        String command = "cmd /c date";
+ 
+try {
+    Process process = Runtime.getRuntime().exec(command);
+ 
+    BufferedWriter writer = new BufferedWriter(
+            new OutputStreamWriter(process.getOutputStream()));
+    writer.write("09-20-14");
+    writer.close();
+ 
+    BufferedReader reader = new BufferedReader(new InputStreamReader(
+            process.getInputStream()));
+    String line;
+    while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+    }
+    reader.close();
+} catch (IOException e) {
+    e.printStackTrace();
+}
+    }
+
+}
